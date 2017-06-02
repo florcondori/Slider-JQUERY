00876dc9f8785c1bb63960662d22d2cb83b03432
flor condori <condorihuamanif@gmail.com>
2017-06-02 18:05:35 -0500

    cambiando li cada 4 seegundos

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,1 +1,7 @@
-app
+$(document).ready(function(){
+	$("ul li:gt(1)").hide();
+
+	setInterval(function(){
+		$("ul li:first-child").fadeOut(0).next("li").fadeIn(1000).end().appendTo("ul");
+	}, 4000);
+});
