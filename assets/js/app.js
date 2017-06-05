90e33db13133fa21a0fae3e45faf7ac2cde497de
flor condori <condorihuamanif@gmail.com>
2017-06-05 08:18:28 -0500

    funcionalidad next

--- assets/js/app.js
+++ assets/js/app.js
@@ -7,12 +7,28 @@ $(document).ready(function(){
 
 	var controles = $(".controles a");
 	controles.each(function(index){
-		$(this).on('click',function(e){
+		$(this).on("click",function(e){
 			e.preventDefault();
 			$(this).parent().children().removeClass("active");
 			$(this).addClass("active");
 			$(".slider-img div:nth-child("+(index+1)+")").siblings().hide();
 			$(".slider-img div:nth-child("+(index+1)+")").fadeIn(1000);
 		});
+	});
+
+	var prev = $("#prev");
+	var next = $("#next");
+
+	prev.on("click", function(e){
+		e.preventDefault();
+		$(".slider-img div:first-child").fadeOut(0).end();
+		$(".slider-img div:last-child").fadeIn(600);
+
+	});
+
+	next.on("click", function(e){
+		e.preventDefault();
+		$(".slider-img div:first-child").fadeOut(0).next("div").fadeIn(600).end().appendTo(".slider-img");
+				
 	});
 });
