2de3970038207dba542e9acebed7a59e619eec25
flor condori <condorihuamanif@gmail.com>
2017-06-05 06:13:12 -0500

    cambiando de imagen con las anclas

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,7 +1,17 @@
 $(document).ready(function(){
-	$("ul li:gt(1)").hide();
+	$(".slider-img div:gt(0)").hide();
 
-	setInterval(function(){
+	/*setInterval(function(){
 		$("ul li:first-child").fadeOut(0).next("li").fadeIn(1000).end().appendTo("ul");
-	}, 4000);
+	}, 5000);*/
+
+	var controles = $(".controles a");
+	controles.each(function(index){
+		$(this).on('click',function(e){
+			e.preventDefault();
+			$(this).parent().children().removeClass("active");
+			$(this).addClass("active");
+			$(".slider-img div:nth-child("+(index+1)+")").fadeIn(1000);
+		});
+	});
 });
